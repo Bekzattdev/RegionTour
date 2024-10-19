1dda6610f9c27840b41b12d25864430c8c5fe294
Bekzattdev <alpberlinfox@gmail.com>
2024-10-19 15:15:18 +0600

    setted deploy

--- src/appPages/site/components/pages/RegionDetails/Events/EventPage.tsx
+++ src/appPages/site/components/pages/RegionDetails/Events/EventPage.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import WelcomeDetails from "../WelcomeDetails";
 import Poster from "./Poster";
-import PosterPlaces from "./PosterPlaces";
 import PlacesDetail from "../PlacesDetail";
 
 const EventPage = () => {
