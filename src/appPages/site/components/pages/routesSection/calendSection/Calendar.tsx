417ee0b7bb92edb4f99c30c6dfeac83dbc01eb92
Bekzattdev <alpberlinfox@gmail.com>
2024-10-19 15:29:25 +0600

    fixed errors 12

--- src/appPages/site/components/pages/routesSection/calendSection/Calendar.tsx
+++ src/appPages/site/components/pages/routesSection/calendSection/Calendar.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
 const MyCalendar: React.FC = () => {
-  const [date, setDate] = React.useState<Date | Date[]>(new Date());
+  const [date, setDate] = useState<Date | [Date, Date] | null>(new Date());
+
+  const handleDayClick = (
+    value: Date,
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    setDate(value);
+  };
 
   return (
     <div
       style={{ display: "flex", justifyContent: "center", marginTop: "30px" }}
     >
-      <Calendar onChange={setDate} value={date} />
+      <Calendar onClickDay={handleDayClick} value={date} />
     </div>
   );
 };
